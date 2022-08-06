1279ef411528df8d948ce76269f037413d8f2dc8
Faith <fhgray96@gmail.com>
2022-08-05 21:55:40 -0500

    add psuedocode

--- src/App.jsx
+++ src/App.jsx
@@ -12,3 +12,8 @@ function App() {
 }
 
 export default App
+
+// Our application will be made of two components, App and Overview. Your application should render an input field and a submit button. With the submit button, you can add the content from your input to a “tasks array” that is managed in state. (We will use class components for this example because we haven’t introduced hooks in this section yet). Finally, for each task in the tasks array, an HTML list element should be rendered
+
+// The page will have a form input to put in tasks and a submit button
+//once a task is submitted it will be added to an array of tasks. The overview component will list all of the tasks and the app component will push the tasks to the page using state.
